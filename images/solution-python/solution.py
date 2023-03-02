60f267c9a3e7d7971fb7187652cd08605983d81e
Anna Nikolaides <avvanik@Annas-MacBook-Air.local>
2023-03-02 11:55:29 +0800

    v1

--- images/solution-python/solution.py
+++ images/solution-python/solution.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python
 
-import csv
 import json
 import sqlalchemy
+import pandas as pd
 
 # connect to the database
 engine = sqlalchemy.create_engine("mysql://codetest:swordfish@database/codetest")
 connection = engine.connect()
 
-metadata = sqlalchemy.schema.MetaData(engine)
+people_df = pd.read_csv('/data/people.csv')
+people_df.to_sql('people', con=engine, if_exists='append', index=True)
 
-# make an ORM object to refer to the table
-Example = sqlalchemy.schema.Table('examples', metadata, autoload=True, autoload_with=engine)
+places_df = pd.read_csv('/data/places.csv')
+places_df.to_sql('places', con=engine, if_exists='append', index=True)
 
-# read the CSV data file into the table
-with open('/data/example.csv') as csv_file:
-  reader = csv.reader(csv_file)
-  next(reader)
-  for row in reader: 
-    connection.execute(Example.insert().values(name = row[0]))
+# people_df.to_json(r'/data/sample_output.json')
 
-# output the table to a JSON file
-with open('/data/example_python.json', 'w') as json_file:
-  rows = connection.execute(sqlalchemy.sql.select([Example])).fetchall()
-  rows = [{'id': row[0], 'name': row[1]} for row in rows]
-  json.dump(rows, json_file, separators=(',', ':'))
+query = "SELECT COUNT(*), a.country FROM places as a RIGHT JOIN people as p ON p.place_of_birth = " \
+        "a.city GROUP BY " \
+        "a.country"
+
+output = connection.execute(query)
+df = pd.DataFrame(output.fetchall())
+#df = df.set_index("country")
+
+dictionary = df.set_index('country').to_dict()['COUNT(*)']
+
+with open("/data/sample_output.json", "w") as outfile:
+    json.dump(dictionary, outfile)
+
+#df.to_json(r'/data/sample_output.json')
+
+print("done")
