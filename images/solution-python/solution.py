d6f7a73529323c5206dee55599823fc440ac15b6
Anna Nikolaides <avvanik@Annas-MacBook-Air.local>
2023-03-02 13:55:24 +0800

    v2

--- images/solution-python/solution.py
+++ images/solution-python/solution.py
@@ -8,27 +8,26 @@ import pandas as pd
 engine = sqlalchemy.create_engine("mysql://codetest:swordfish@database/codetest")
 connection = engine.connect()
 
-people_df = pd.read_csv('/data/people.csv')
-people_df.to_sql('people', con=engine, if_exists='append', index=True)
+# set list for csvs
+csv_names = ['people', 'places']
+# set names for primary_key
+id_names = ['person', 'city']
 
-places_df = pd.read_csv('/data/places.csv')
-places_df.to_sql('places', con=engine, if_exists='append', index=True)
+# load data to db + define schema
+for csv_name, id_name in zip(csv_names, id_names):
+    df = pd.read_csv(f'/data/{csv_name}.csv')
+    df[f'{id_name}_id'] = range(1, 1 + len(df))
+    df[f'{id_name}_id'] = f'{id_name[0]}' + df[f'{id_name}_id'].astype(str)
+    df.to_sql(f'{csv_name}', con=engine, if_exists='replace', index=False)
 
-# people_df.to_json(r'/data/sample_output.json')
-
-query = "SELECT COUNT(*), a.country FROM places as a RIGHT JOIN people as p ON p.place_of_birth = " \
+# query list of countries + people born in country
+query = "SELECT COUNT(*) as people_count, a.country FROM places as a RIGHT JOIN people as p ON p.place_of_birth = " \
         "a.city GROUP BY " \
         "a.country"
 
 output = connection.execute(query)
-df = pd.DataFrame(output.fetchall())
-#df = df.set_index("country")
 
-dictionary = df.set_index('country').to_dict()['COUNT(*)']
-
-with open("/data/sample_output.json", "w") as outfile:
+# safe as json
+dictionary = pd.DataFrame(output.fetchall()).set_index('country').to_dict()['people_count']
+with open("/data/summary_output.json", "w") as outfile:
     json.dump(dictionary, outfile)
-
-#df.to_json(r'/data/sample_output.json')
-
-print("done")
